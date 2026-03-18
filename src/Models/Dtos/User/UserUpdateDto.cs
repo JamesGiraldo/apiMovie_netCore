09ba06dd925fc0685321600dfed6cd3ae03144fa
jamesgiraldo <jamesgiiraldo@gmail.com>
2026-03-18 00:09:16 -0500

    FIX: updated userUpdatedDto

--- src/Models/Dtos/User/UserUpdateDto.cs
+++ src/Models/Dtos/User/UserUpdateDto.cs
@@ -12,6 +12,10 @@ public class UserUpdateDto
     [MaxLength(100, ErrorMessage = "The name must be less than 100 characters")]
     public string Name { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "The last name is required")]
+    [MaxLength(100, ErrorMessage = "The last name must be less than 100 characters")]
+    public string? LastName { get; set; } = null;
+
     [Required(ErrorMessage = "The username is required")]
     [MaxLength(100, ErrorMessage = "The username must be less than 100 characters")]
     public string UserName { get; set; } = string.Empty;
@@ -19,7 +23,7 @@ public class UserUpdateDto
     [Phone(ErrorMessage = "The phone number is not valid")]
     [Column(TypeName = "varchar(15)")]
     [MaxLength(15, ErrorMessage = "The phone number must be less than 15 characters")]
-    public string PhoneNumber { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; } = null;
 
     [Required(ErrorMessage = "The email is required")]
     [EmailAddress(ErrorMessage = "The email is not valid")]
